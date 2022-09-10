ce1683879ec0830b5865220946b2912d8fb85668
v_sunyanyun <v_sunyanyun@baidu.com>
2022-09-10 17:53:54 +0800

    fix: 更新自定义头部导航传参

--- src/components/topNav.vue
+++ src/components/topNav.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="status-bar" :style="{height: `${statusBarHeight}px`}"></div>
     <nut-navbar :left-show="leftShow" :title="title" :style="{height: titleBarHeight + 'px'}">
-      <template #left>
+      <template #left v-if="!leftShow">
         <nut-icon name="my2" color="#5d5fef" @click="goPathHandle"></nut-icon>
       </template>
     </nut-navbar>
 </template>
 <script lang="ts" setup>
-const props = defineProps(['leftShow', 'title'])
+const props = withDefaults(defineProps<{
+    leftShow?: boolean,
+    title: string
+  }>(), {
+    leftShow: true,
+    title: ''
+  })
+
 const emit = defineEmits(['goPathHandle'])
 import { useNavInfo } from '@/hooks/useNavInfo'
+
+import '@/assets/styles/navBar.scss'
+
 console.log(props.leftShow, 'leftShow----nav')
 const {statusBarHeight, titleBarHeight} = useNavInfo()
 const goPathHandle = () => {
